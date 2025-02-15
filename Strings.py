aeb3db4aac292788ed7a17905a30162a144500b8
Heidi Christina Thysen <Heidi.Thysen@nhh.no>
2025-02-15 11:39:23 +0100

    New String Challenge

--- Strings.py
+++ Strings.py
@@ -20,3 +20,35 @@ def merge_the_tools(string, k):
 if __name__ == '__main__':
     string, k = input(), int(input())
     merge_the_tools(string, k)
+
+# Kevin and Stuart want to play the 'The Minion Game'.
+
+# Game Rules
+
+# Both players are given the same string, S.
+# Both players have to make substrings using the letters of the string S.
+# Stuart has to make words starting with consonants.
+# Kevin has to make words starting with vowels.
+# The game ends when both players have made all possible substrings.
+
+# Scoring
+# A player gets +1 point for each occurrence of the substring in the string S.
+def minion_game(string):
+    Stuart = 0
+    Kevin = 0
+    v = "aeiouAEIOU"
+    for s in range(len(string)): #This is the starting point of the substring
+        if string[s:s+1] in v:
+            Kevin = Kevin + len(string[s:])
+        else: 
+            Stuart = Stuart + len(string[s:])
+    if (Stuart > Kevin):
+        print("Stuart "+str(Stuart))
+    elif (Stuart < Kevin):
+        print("Kevin "+str(Kevin))
+    else:
+        print("Draw")
+    
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
