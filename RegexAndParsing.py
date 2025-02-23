ffae05998fe11009f0238a2bc7ea1140eab20ab2
HCThysen <heidi.thysen@gmail.com>
2025-02-22 20:16:31 -0800

    Now with the exercise saved

---
+++ RegexAndParsing.py
@@ -0,0 +1,18 @@
+#Task
+
+# You are given a string S. Your task is to find the first occurrence of an alphanumeric character in S (read from left to right) that has consecutive repetitions.
+
+# Input Format:
+# A single line of input containing the string S.
+
+# Output Format:
+# Print the first occurrence of the repeating character. If there are no repeating characters, print -1.
+import re
+string = input()
+#re.search(pattern, string) returns 1 if the pattern is found in the string
+m = re.search(r'([a-zA-Z0-9])\1',string)
+if m:
+    print(m.groups()[0])
+else:
+    print(-1)
+
