922f115d0d18dfafd2e7b218e46248f5c25f22cd
HCThysen <heidi.thysen@gmail.com>
2025-02-26 21:33:09 -0800

    another data types exercise

--- BasicDataTypes.py
+++ BasicDataTypes.py
@@ -39,4 +39,21 @@ if __name__ == '__main__':
     n = int(input())
     
     print( [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n] )
-    
+
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
+# Input Format
+# The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+
+# Output Format
+#Print the runner-up score.
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    first = second = -float('inf')
+    for i in arr:
+        if i > first:
+            second = first
+            first = i
+        elif first > i > second:
+            second = i
+    print(second)
